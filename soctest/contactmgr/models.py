8f6ac2eaa1d810172d9b86da2439d853916ce2d9
RussMax783 <russ.max783@gmail.com>
2016-04-20 12:08:50 -0600

    Create contact models. Set up admin to display models

--- soctest/contactmgr/models.py
+++ soctest/contactmgr/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=255)
+    postal_code = models.CharField(max_length=255)
+    country = models.CharField(max_length=255)
+    phone = models.CharField(max_length=255)
+
+
+# NOTE: If contacts were always going to be in the US i would model
+#       something like this, where i get pre made validations
+
+# from django.db import models
+# from localflavor.us.us_states import STATE_CHOICES
+# from localflavor.us.models import PhoneNumberField, USZipCodeField
+#
+#
+# class Contact(models.Model):
+#     name = models.CharField(max_length=255)
+#     address = models.CharField(max_length=255)
+#     city = models.CharField(max_length=255)
+#     state = models.CharField(max_length=2, choices=STATE_CHOICES)
+#     postal_code = USZipCodeField(max_length=255)
+#     country = models.CharField(max_length=255)
+#     phone = PhoneNumberField(max_length=255)
--- soctest/soctest/settings.py
+++ soctest/soctest/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'contactmgr',
 )
 
 MIDDLEWARE_CLASSES = (
